1d1db6f2a344890a7cdacaee73ceea0916f9a56a
Philippe Saint Jacques <philippe@cleany.fr>
2017-10-02 17:12:54 +0200

    infiniList: factorization of the code
    We factorizing the code, in order to have less code.

--- src/mui/list/InfiniteList.js
+++ src/mui/list/InfiniteList.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { parse, stringify } from 'query-string';
 import { push as pushAction } from 'react-router-redux';
@@ -21,6 +20,8 @@ import removeKey from '../../util/removeKey';
 import defaultTheme from '../defaultTheme';
 import InfiniteScroll from 'react-infinite-scroller';
 import LinearProgress from 'material-ui/LinearProgress';
+import { List } from './List';
+
 
 const styles = {
     noResults: { padding: 20 },
@@ -30,261 +31,115 @@ const styles = {
     },
 };
 
-/**
- * InfiniteList page component
- *
- * The <InfiniteList> component renders the list layout (title, buttons, filters),
- * and fetches the list of records from the REST API.
- * It then delegates the rendering of the list of records to its child component.
- * Usually, it's a <Datagrid>, responsible for displaying a table with one row for each post.
- *
- * In Redux terms, <List> is a connected component, and <Datagrid> is a dumb component.
- *
- * Props:
- *   - title
- *   - perPage
- *   - sort
- *   - filter (the permanent filter to apply to the query)
- *   - actions
- *   - filters (a React Element used to display the filter form)
- *   - pagination
- *
- * @example
- *     const PostFilter = (props) => (
- *         <Filter {...props}>
- *             <TextInput label="Search" source="q" alwaysOn />
- *             <TextInput label="Title" source="title" />
- *         </Filter>
- *     );
- *     export const PostList = (props) => (
- *         <InfiniteList {...props}
- *             title="List of posts"
- *             sort={{ field: 'published_at' }}
- *             filter={{ is_published: true }}
- *             filters={<PostFilter />}
- *         >
- *             <Datagrid>
- *                 <TextField source="id" />
- *                 <TextField source="title" />
- *                 <EditButton />
- *             </Datagrid>
- *         </InfiniteList>
- *     );
- */
-export class InfiniteList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { key: 0 };
-    }
+export class InfiniteList extends List {
+  constructor(props, state) {
+    super(props, state)
+    console.log(props);
+    console.log(state);
+  }
 
-    componentDidMount() {
-        this.updateData();
-        if (Object.keys(this.props.query).length > 0) {
-            this.props.query.page = 0;
-            this.props.changeListParams(this.props.resource, this.props.query);
-        }
-    }
+  componentDidMount() {
+      this.updateData();
+      if (Object.keys(this.props.query).length > 0) {
+          this.props.query.page = 0;
+          this.props.changeListParams(this.props.resource, this.props.query);
+      }
+  }
 
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.resource !== this.props.resource
-         || nextProps.query.sort !== this.props.query.sort
-         || nextProps.query.order !== this.props.query.order
-         || nextProps.query.page !== this.props.query.page
-         || nextProps.query.filter !== this.props.query.filter) {
-            this.updateData(Object.keys(nextProps.query).length > 0 ? nextProps.query : nextProps.params);
-        }
-        if (nextProps.data !== this.props.data && this.fullRefresh) {
-            this.fullRefresh = false;
-            this.setState({ key: this.state.key + 1 });
-        }
-    }
+  getQuery() {
+      const query = Object.keys(this.props.query).length > 0 ?
+      Object.assign({}, this.props.query, {page: this.props.params.page}) : { ...this.props.params };
+      if (!query.sort) {
+          query.sort = this.props.sort.field;
+          query.order = this.props.sort.order;
+      }
+      if (!query.perPage) {
+          query.perPage = this.props.perPage;
+      }
+      return query;
+  }
 
-    shouldComponentUpdate(nextProps, nextState) {
-        if (
-            nextProps.isLoading === this.props.isLoading
-         && nextProps.width === this.props.width
-         && nextState === this.state) {
-            return false;
-        }
-        return true;
-    }
+  updateData(query) {
+      const params = query || this.getQuery();
+      const { sort, order, page, perPage, filter } = params;
+      const pagination = { page: parseInt(page, 10), perPage: parseInt(perPage, 10) };
+      const permanentFilter = this.props.filter;
+      this.props.crudGetList(this.props.resource, pagination, { field: sort, order }, { ...filter, ...permanentFilter }, false, true);
+  }
 
-    getBasePath() {
-      return this.props.location.pathname;
-    }
+  getNextPage() {
+       this.changeParams({type: 'INC_PAGE'})
+  }
 
-    refresh = (event) => {
-        event.stopPropagation();
-        this.fullRefresh = true;
-        this.updateData();
-    }
+  render() {
+      const { filters, pagination = <DefaultPagination />, actions = <DefaultActions />, resource, hasCreate, title, data, ids, total, children, isLoading, translate, theme } = this.props;
+      const { key } = this.state;
+      const query = this.getQuery();
+      const filterValues = query.filter;
+      const basePath = this.props.location.pathname;
+      const loader = <LinearProgress mode="indeterminate" />;
+      const resourceName = translate(`resources.${resource}.name`, {
+          smart_count: 2,
+          _: inflection.humanize(inflection.pluralize(resource)),
+      });
+      const defaultTitle = translate('aor.page.list', { name: `${resourceName}` });
+      const titleElement = <Title title={title} defaultTitle={defaultTitle} />;
+      const muiTheme = getMuiTheme(theme);
+      const prefix = autoprefixer(muiTheme);
 
-    /**
-     * Merge list params from 3 different sources:
-     *   - the query string
-     *   - the params stored in the state (from previous navigation)
-     *   - the props passed to the List component
-     */
-    getQuery() {
-        const query = Object.keys(this.props.query).length > 0 ?
-        Object.assign({}, this.props.query, {page: this.props.params.page}) : { ...this.props.params };
-        if (!query.sort) {
-            query.sort = this.props.sort.field;
-            query.order = this.props.sort.order;
-        }
-        if (!query.perPage) {
-            query.perPage = this.props.perPage;
-        }
-        return query;
-    }
-
-    updateData(query) {
-        const params = query || this.getQuery();
-        const { sort, order, page, perPage, filter } = params;
-        const pagination = { page: parseInt(page, 10), perPage: parseInt(perPage, 10) };
-        const permanentFilter = this.props.filter;
-        this.props.crudGetList(this.props.resource, pagination, { field: sort, order }, { ...filter, ...permanentFilter }, false, true);
-    }
-
-    getNextPage() {
-         this.changeParams({type: 'INC_PAGE'})
-    }
-
-    setSort = sort => this.changeParams({ type: SET_SORT, payload: sort });
-
-    setFilters = filters => this.changeParams({ type: SET_FILTER, payload: filters });
-
-    showFilter = (filterName, defaultValue) => {
-        this.setState({ [filterName]: true });
-        if (typeof defaultValue !== 'undefined') {
-            this.setFilters({ ...this.props.filterValues, [filterName]: defaultValue });
-        }
-    }
-
-    hideFilter = (filterName) => {
-        this.setState({ [filterName]: false });
-        const newFilters = removeKey(this.props.filterValues, filterName);
-        this.setFilters(newFilters);
-    }
-
-    changeParams(action) {
-        const newParams = queryReducer(this.getQuery(), action);
-        this.props.push({ ...this.props.location, search: `?${stringify({ ...newParams, filter: JSON.stringify(newParams.filter) })}` });
-        this.props.changeListParams(this.props.resource, newParams);
-    }
-
-    render() {
-        const { filters, pagination = <DefaultPagination />, actions = <DefaultActions />, resource, hasCreate, title, data, ids, total, children, isLoading, translate, theme } = this.props;
-        const { key } = this.state;
-        const query = this.getQuery();
-        const filterValues = query.filter;
-        const basePath = this.getBasePath();
-        const loader = <LinearProgress mode="indeterminate" />;
-        const resourceName = translate(`resources.${resource}.name`, {
-            smart_count: 2,
-            _: inflection.humanize(inflection.pluralize(resource)),
-        });
-        const defaultTitle = translate('aor.page.list', { name: `${resourceName}` });
-        const titleElement = <Title title={title} defaultTitle={defaultTitle} />;
-        const muiTheme = getMuiTheme(theme);
-        const prefix = autoprefixer(muiTheme);
-
-        return (
-            <div className="list-page">
-                <Card style={{ opacity: isLoading ? 0.8 : 1 }}>
-                    <div style={prefix(styles.header)} >
-                        <ViewTitle title={titleElement} />
-                        {actions && React.cloneElement(actions, {
-                            resource,
-                            filters,
-                            filterValues,
-                            basePath,
-                            hasCreate,
-                            displayedFilters: this.state,
-                            showFilter: this.showFilter,
-                            refresh: this.refresh,
-                            theme,
-                        })}
-                    </div>
-                    {filters && React.cloneElement(filters, {
-                        resource,
-                        hideFilter: this.hideFilter,
-                        filterValues,
-                        displayedFilters: this.state,
-                        setFilters: this.setFilters,
-                        context: 'form',
-                    })}
-                    <InfiniteScroll
-                      pageStart={1}
-                      initialLoad={true}
-                      loadMore={this.getNextPage.bind(this)}
-                      hasMore={this.props.hasMore}
-                      loader={loader}
-                    >
-                    { isLoading || total > 0 ?
-                      <div key={key} className='list-item' style={prefix(styles.listItem)}>
-                        {children && React.cloneElement(children, {
+      return (
+          <div className="list-page">
+              <Card style={{ opacity: isLoading ? 0.8 : 1 }}>
+                  <div style={prefix(styles.header)} >
+                      <ViewTitle title={titleElement} />
+                      {actions && React.cloneElement(actions, {
                           resource,
-                          ids,
-                          data,
-                          currentSort: { field: query.sort, order: query.order },
+                          filters,
+                          filterValues,
                           basePath,
-                          isLoading,
-                          setSort: this.setSort,
-                        })}
-                      </div>
-                      :
-                      <CardText style={styles.noResults}>{translate('aor.navigation.no_results')}</CardText>
-                    }
-                    </InfiniteScroll>
-                </Card>
-            </div>
-        );
-    }
+                          hasCreate,
+                          displayedFilters: this.state,
+                          showFilter: this.showFilter,
+                          refresh: this.refresh,
+                          theme,
+                      })}
+                  </div>
+                  {filters && React.cloneElement(filters, {
+                      resource,
+                      hideFilter: this.hideFilter,
+                      filterValues,
+                      displayedFilters: this.state,
+                      setFilters: this.setFilters,
+                      context: 'form',
+                  })}
+                  <InfiniteScroll
+                    pageStart={1}
+                    initialLoad={true}
+                    loadMore={this.getNextPage.bind(this)}
+                    hasMore={this.props.hasMore}
+                    loader={loader}
+                  >
+                  { isLoading || total > 0 ?
+                    <div key={key} className='list-item' style={prefix(styles.listItem)}>
+                      {children && React.cloneElement(children, {
+                        resource,
+                        ids,
+                        data,
+                        currentSort: { field: query.sort, order: query.order },
+                        basePath,
+                        isLoading,
+                        setSort: this.setSort,
+                      })}
+                    </div>
+                    :
+                    <CardText style={styles.noResults}>{translate('aor.navigation.no_results')}</CardText>
+                  }
+                  </InfiniteScroll>
+              </Card>
+          </div>
+      );
+  }
 }
-
-InfiniteList.propTypes = {
-    // the props you can change
-    title: PropTypes.any,
-    filter: PropTypes.object,
-    filters: PropTypes.element,
-    pagination: PropTypes.element,
-    actions: PropTypes.element,
-    perPage: PropTypes.number.isRequired,
-    sort: PropTypes.shape({
-        field: PropTypes.string,
-        order: PropTypes.string,
-    }),
-    children: PropTypes.element.isRequired,
-    // the props managed by admin-on-rest
-    changeListParams: PropTypes.func.isRequired,
-    crudGetList: PropTypes.func.isRequired,
-    data: PropTypes.object, // eslint-disable-line react/forbid-prop-types
-    filterValues: PropTypes.object, // eslint-disable-line react/forbid-prop-types
-    hasCreate: PropTypes.bool.isRequired,
-    ids: PropTypes.array,
-    isLoading: PropTypes.bool.isRequired,
-    location: PropTypes.object.isRequired,
-    path: PropTypes.string,
-    params: PropTypes.object.isRequired,
-    push: PropTypes.func.isRequired,
-    query: PropTypes.object.isRequired,
-    resource: PropTypes.string.isRequired,
-    total: PropTypes.number.isRequired,
-    translate: PropTypes.func.isRequired,
-    theme: PropTypes.object.isRequired,
-};
-
-InfiniteList.defaultProps = {
-    filter: {},
-    filterValues: {},
-    perPage: 10,
-    sort: {
-        field: 'id',
-        order: SORT_DESC,
-    },
-    theme: defaultTheme,
-};
 
 const getLocationSearch = props => props.location.search;
 const getQuery = createSelector(
