e8a2e6b56504cc7ba3a4c1fc4630ccaf23e36fa8
Thomas Tabouelle <thomas@cleany.fr>
2017-10-16 12:14:13 +0200

    Field: add a props for disable link on PhoneField and EmailField
    In order to fancy our lists in the app, we want no link in it.

--- src/mui/field/EmailField.js
+++ src/mui/field/EmailField.js
@@ -3,24 +3,33 @@ import PropTypes from 'prop-types';
 import get from 'lodash.get';
 import pure from 'recompose/pure';
 
-const EmailField = ({ source, record = {}, elStyle }) => (
-    <a style={elStyle} href={`mailto:${get(record, source)}`}>
-        {get(record, source)}
-    </a>
-);
+const EmailField = ({ source, record = {}, elStyle, linkType }) => {
+  if (linkType) {
+    return (
+      <a style={elStyle} href={`mailto:${get(record, source)}`}>
+      {get(record, source)}
+      </a>
+    );
+  }
+  return (
+    <span style={elStyle}>{get(record, source)}</span>
+  );
+}
 
 EmailField.propTypes = {
-    addLabel: PropTypes.bool,
-    elStyle: PropTypes.object,
-    label: PropTypes.string,
-    record: PropTypes.object,
-    source: PropTypes.string.isRequired,
+  addLabel: PropTypes.bool,
+  elStyle: PropTypes.object,
+  label: PropTypes.string,
+  record: PropTypes.object,
+  source: PropTypes.string.isRequired,
+  linkType: PropTypes.bool,
 };
 
 const PureEmailField = pure(EmailField);
 
 PureEmailField.defaultProps = {
-    addLabel: true,
+  addLabel: true,
+  linkType: true,
 };
 
 export default PureEmailField;
