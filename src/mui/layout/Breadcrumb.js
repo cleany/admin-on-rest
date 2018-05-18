e35f5c2ba8c6a12661ec796dce29c5fa0583bab7
killui <quentin.lemoine12@gmail.com>
2018-05-18 10:38:30 +0200

    mui: add options Breadcrumbs

--- src/mui/layout/Breadcrumb.js
+++ src/mui/layout/Breadcrumb.js
@@ -1,44 +1,37 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import inflection from 'inflection';
-import translate from '../../i18n/translate';
-
 
 class Breadcrumb extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hover: false,
-    };
-    this.hoverOn = this.hoverOn.bind(this);
-    this.hoverOff = this.hoverOff.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            hover: false,
+        };
+        this.hoverOn = this.hoverOn.bind(this);
+        this.hoverOff = this.hoverOff.bind(this);
+    }
 
-  hoverOn() {
-    this.setState({ hover: true });
-  }
+    hoverOn() {
+        this.setState({ hover: true });
+    }
 
-  hoverOff() {
-    this.setState({ hover: false });
-  }
+    hoverOff() {
+        this.setState({ hover: false });
+    }
 
-  render() {
-    const {
-      url,
-      text,
-      styles,
-    } = this.props;
-    return (
-        <a
-          href={url}
-          style={ this.state.hover ? styles.hover : styles.noHover}
-          onMouseEnter={this.hoverOn}
-          onMouseLeave={this.hoverOff}
-        >
-            {text}
-        </a>
-    );
-  }
+    render() {
+        const { url, text, styles } = this.props;
+        return (
+            <a
+                href={url}
+                style={this.state.hover ? styles.hover : styles.noHover}
+                onMouseEnter={this.hoverOn}
+                onMouseLeave={this.hoverOff}
+            >
+                {text}
+            </a>
+        );
+    }
 }
 
 Breadcrumb.propTypes = {
--- src/mui/layout/Breadcrumbs.js
+++ src/mui/layout/Breadcrumbs.js
@@ -5,43 +5,40 @@ import translate from '../../i18n/translate';
 import Breadcrumb from '../layout/Breadcrumb';
 
 class Breadcrumbs extends Component {
-  render() {
-    const {
-      data,
-      display,
-      resource,
-      view,
-      translate,
-      styles,
-    } = this.props;
-    const resourceName = translate(
-        `resources.${resource}.name`,
-        { smart_count: 2,
-        _: inflection.humanize(inflection.singularize(resource)),
-    });
-    if (!display) return null;
+    render() {
+        const { data, display, resource, view, translate, styles } = this.props;
+        const resourceName = translate(`resources.${resource}.name`, {
+            smart_count: 2,
+            _: inflection.humanize(inflection.singularize(resource)),
+        });
+        if (!display) return null;
 
-    return (
-        <div className="breadcrumbs aor-no-print">
-            <Breadcrumb
-              url={`#/${resource}`}
-              text={`${resourceName} /`}
-              styles={styles}
-            />
-            {view === 'edit' && data &&
+        return (
+            <div className="breadcrumbs aor-no-print">
                 <Breadcrumb
-                  url={`#/${resource}/${data.id}`}
-                  text={
-                    !!data.account_name
-                    ? (` ${data.account_name} / `)
-                    : (!!data.name ? ` ${data.name} /` : ` ${data.fullname} /`)
-                  }
-                  styles={styles}
+                    url={`#/${resource}`}
+                    text={`${resourceName} /`}
+                    styles={styles}
                 />
-            }
-        </div>
-    );
-  }
+                {view === 'edit' &&
+                data && (
+                    <Breadcrumb
+                        url={`#/${resource}/${data.id}`}
+                        text={
+                            data.account_name ? (
+                                ` ${data.account_name} / `
+                            ) : data.name ? (
+                                ` ${data.name} /`
+                            ) : (
+                                ` ${data.fullname} /`
+                            )
+                        }
+                        styles={styles}
+                    />
+                )}
+            </div>
+        );
+    }
 }
 
 Breadcrumbs.propTypes = {
